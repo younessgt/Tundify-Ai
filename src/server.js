227f0db326b23da3f6f1e1ef09467b829781fcbd
younessgt <youness.merzak123@gmail.com>
2024-09-26 17:31:42 +0100

    set up MongoDb Database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const logger = require("./configs/logger");
+const mongoose = require("mongoose");
 
 // handling uncaught exceptions
 process.on("uncaughtException", (err) => {
@@ -11,17 +12,37 @@ process.on("uncaughtException", (err) => {
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const PORT = process.env.PORT || 8000;
+// Connect to the Atlas MongoDB database
+mongoose
+  .connect(
+    process.env.DATABASE_ATLAS.replace(
+      "<password>",
+      process.env.DATABASE_PASSWORD
+    )
+  )
+  .then(() => {
+    logger.info("Connected to the Atlas MongoDB database");
+  })
+  .catch((err) => {
+    logger.error(
+      `Error connecting to the Atlas MongoDB database: ${err.message}`
+    );
+    process.exit(1);
+  });
 
-// console.log(process.env.NODE_ENV);
+// activate mongoose debugging mode on development environment
+
+if (process.env.NODE_ENV === "development") {
+  mongoose.set("debug", true);
+}
 
 // Start the server on the specified port
+const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 // handling unhandled promise rejections
-
 process.on("unhandledRejection", (err) => {
   logger.error("Unhandled Rejection: ", err.message);
   server.close(() => {
