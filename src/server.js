eee3287cf1a6a3a726021f1ee068ee58d18604a7
younessgt <youness.merzak123@gmail.com>
2024-09-26 15:26:21 +0100

    handling uncaughtExceptions unhandledRejection and the SIGTERM signal

--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
 const dotenv = require("dotenv");
+const logger = require("./configs/logger");
+
+// handling uncaught exceptions
+process.on("uncaughtException", (err) => {
+  logger.error(`Uncaught Exception: ${err.message}`);
+  process.exit(1);
+});
+
+// Load environment variables from the .env file
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
@@ -9,4 +18,21 @@ const PORT = process.env.PORT || 8000;
 // Start the server on the specified port
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// handling unhandled promise rejections
+
+process.on("unhandledRejection", (err) => {
+  logger.error("Unhandled Rejection: ", err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// handling SIGTERM signal
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM received. Shutting down gracefully.");
+  server.close(() => {
+    process.exit(0);
+  });
 });
