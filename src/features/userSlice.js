2b08240f8a3d46b58b92fa2c9c28c4db778511df
younessgt <youness.merzak123@gmail.com>
2024-10-02 12:40:18 +0100

    setup redux toolkit

---
+++ src/features/userSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { REHYDRATE } from "redux-persist";
+
+const initialState = {
+  status: "1",
+  error: "2",
+  user: {
+    id: "",
+    name: "kamal",
+    email: "",
+    picture: "",
+    token: "",
+  },
+};
+
+export const userSlice = createSlice({
+  name: "userState",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.status = "";
+      state.error = "";
+      state.user = {
+        id: "",
+        name: "",
+        email: "",
+        picture: "",
+        token: "",
+      };
+    },
+  },
+});
+
+export const { logout } = userSlice.actions;
+
+export default userSlice.reducer;
