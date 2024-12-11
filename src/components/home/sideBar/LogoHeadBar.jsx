7b286e37f290f2c8b9a71049539afc9856848808
younessgt <youness.merzak123@gmail.com>
2024-12-11 14:45:17 +0100

    add sidebar part

---
+++ src/components/home/sideBar/LogoHeadBar.jsx
@@ -0,0 +1,10 @@
+import { FireFilled } from "@ant-design/icons";
+export default function LogoHeadBar() {
+  return (
+    <div className="flex items-center justify-center text-white px-[10px]">
+      <div className="w-[30px] h-[30px] flex items-center justify-center text-[25px]">
+        <FireFilled />
+      </div>
+    </div>
+  );
+}
